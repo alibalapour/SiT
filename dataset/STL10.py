287d6385a204d8b78c67dab9fee6f1a44985d6f7
Ali Balapour <42287060+alibalapour@users.noreply.github.com>
2021-11-14 00:17:42 +0330

    Update STL10.py

--- dataset/STL10.py
+++ dataset/STL10.py
@@ -42,7 +42,7 @@ class STL10(torchvision.datasets.STL10):
             download: bool = False, num_imgs_per_cat = None, training_mode = 'SSL'
     ) -> None:
         super(STL10, self).__init__(root, transform=transform,
-                                    target_transform=target_transform)
+                                    target_transform=target_transform, download=True)
         
         self.training_mode = training_mode
         
